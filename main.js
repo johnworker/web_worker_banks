78bace377be53d03f9c2c672ec19c082e8ac571f
johnworker <besscomdark@gmail.com>
2024-05-21 17:57:05 +0800

    調整

--- main.js
+++ main.js
@@ -54,7 +54,66 @@ function animateCircle() {
             x: -200, // 固定向左移動的距離，可根據需求調整
             y: 0,
             duration: 5, // 動畫持續2秒
-            
+            onComplete: function () {
+                gsap.delayedCall(3, animateCircle); // 3秒後重新開始動畫
+            },
+            ease: "power1.inOut"
+        }
+    );
+
+    gsap.fromTo(".animate_curcle_four",
+        {
+            scale: 1,
+            opacity: 1,
+            x: 0,
+            y: 0
+        },
+        {
+            scale: 0.65, // 可根據需求調整擴大的比例
+            opacity: 0,
+            x: 200, // 固定向左移動的距離，可根據需求調整
+            y: 180,
+            duration: 5, // 動畫持續2秒
+            onComplete: function () {
+                gsap.delayedCall(3, animateCircle); // 3秒後重新開始動畫
+            },
+            ease: "power1.inOut"
+        },
+    );
+
+    gsap.fromTo(".animate_curcle_five",
+        {
+            scale: 1,
+            opacity: 1,
+            x: 0,
+            y: 0
+        },
+        {
+            scale: 0.65, // 可根據需求調整擴大的比例
+            opacity: 0,
+            x: 200, // 固定向左移動的距離，可根據需求調整
+            y: 20,
+            duration: 5, // 動畫持續2秒
+            onComplete: function () {
+                gsap.delayedCall(3, animateCircle); // 3秒後重新開始動畫
+            },
+            ease: "power1.inOut"
+        }
+    );
+
+    gsap.fromTo(".animate_curcle_six",
+        {
+            scale: 1,
+            opacity: 1,
+            x: 0,
+            y: 0
+        },
+        {
+            scale: 0.65, // 可根據需求調整擴大的比例
+            opacity: 0,
+            x: 200, // 固定向左移動的距離，可根據需求調整
+            y: 0,
+            duration: 5, // 動畫持續2秒
             onComplete: function () {
                 gsap.delayedCall(3, animateCircle); // 3秒後重新開始動畫
             },
