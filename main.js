f5e28f0bcd61378dfc3cc3fd500e7ec18e1100b3
johnworker <besscomdark@gmail.com>
2024-05-22 15:53:54 +0800

    調整

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+// 圖片輪播
+$(document).ready(function () {
+    var $slider = $('.image_mask_box');
+    $slider.slick({
+        dots: false,
+        infinite: true,
+        fade: true,
+        arrows: false,
+        draggable: false,
+        autoplay: true,
+        autoplaySpeed: 5000, // 動畫持續時間為5秒
+        speed: 5000, // 圖片切換速度為5秒
+        pauseOnHover: false,
+        cssEase: 'linear'
+    });
+
+    $slider.on('afterChange', function(event, slick, currentSlide) {
+        $slider.slick('slickPause'); // 停止自動播放
+        setTimeout(function() {
+            $slider.slick('slickPlay'); // 3秒後重新開始播放
+        }, 3000); // 停3秒
+    });
+});
+
 // 圓形符號動畫區
 
 function animateCircle() {
@@ -13,7 +37,7 @@ function animateCircle() {
             opacity: 0,
             x: -200, // 固定向左移動的距離，可根據需求調整
             y: -150,
-            duration: 2, // 動畫持續2秒
+            duration: 5, // 動畫持續5秒
             ease: "power1.inOut",
             repeat: -1, // 無限重複
             repeatDelay: 3, // 每次動畫結束後3秒重新開始
@@ -33,7 +57,7 @@ function animateCircle() {
             opacity: 0,
             x: -200, // 固定向左移動的距離，可根據需求調整
             y: 50,
-            duration: 2, // 動畫持續2秒
+            duration: 5, // 動畫持續5秒
             ease: "power1.inOut",
             repeat: -1, // 無限重複
             repeatDelay: 2, // 每次動畫結束後2秒重新開始
@@ -53,7 +77,7 @@ function animateCircle() {
             opacity: 0,
             x: -200, // 固定向左移動的距離，可根據需求調整
             y: 0,
-            duration: 2, // 動畫持續2秒
+            duration: 5, // 動畫持續5秒
             ease: "power1.inOut",
             repeat: -1, // 無限重複
             repeatDelay: 8, // 每次動畫結束後8秒重新開始
@@ -73,7 +97,7 @@ function animateCircle() {
             opacity: 0,
             x: 200, // 固定向左移動的距離，可根據需求調整
             y: -180,
-            duration: 2, // 動畫持續2秒
+            duration: 5, // 動畫持續5秒
             ease: "power1.inOut",
             repeat: -1, // 無限重複
             repeatDelay: 4, // 每次動畫結束後4秒重新開始
@@ -93,7 +117,7 @@ function animateCircle() {
             opacity: 0,
             x: 200, // 固定向左移動的距離，可根據需求調整
             y: -80,
-            duration: 2, // 動畫持續2秒
+            duration: 5, // 動畫持續5秒
             ease: "power1.inOut",
             repeat: -1, // 無限重複
             repeatDelay: 5, // 每次動畫結束後5秒重新開始
@@ -113,7 +137,7 @@ function animateCircle() {
             opacity: 0,
             x: 200, // 固定向左移動的距離，可根據需求調整
             y: 0,
-            duration: 2, // 動畫持續2秒
+            duration: 5, // 動畫持續5秒
             ease: "power1.inOut",
             repeat: -1, // 無限重複
             repeatDelay: 3, // 每次動畫結束後3秒重新開始
