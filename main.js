bc617774b481a8cb55e2404d838b096e5600f760
johnworker <besscomdark@gmail.com>
2024-06-04 13:57:27 +0800

    調整

--- main.js
+++ main.js
@@ -189,3 +189,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+
+// 選單效果
+$(".menu_btn").click(function () {
+    $(".menu_wrap").toggleClass("active");
+    $(this).toggleClass("active");
+  });
