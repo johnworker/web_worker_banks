42b819c3d8d98f11c3e196668e920b3107c58c12
hunte <besscomdark@gmail.com>
2024-06-24 08:06:41 +0800

    調整

--- main.js
+++ main.js
@@ -159,6 +159,20 @@ function animateCircle() {
 
 animateCircle(); // 初始化動畫
 
+// 螺旋動畫區
+document.addEventListener('DOMContentLoaded', () => {
+    // 初始化SVG路徑動畫
+    gsap.to(".spiral", {
+        strokeDasharray: 400,
+        strokeDashoffset: 400,
+        duration: 4,
+        repeat: -1,
+        ease: "power1.inOut",
+        yoyo: true
+    });
+});
+
+
 
 //關於我們輪播區
 // 圖片輪播
